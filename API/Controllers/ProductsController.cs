6e8231e6a04df9f9275b530285ad59fdbf492a27
shaharukhgit <shaharukhpathan786@gmail.com>
2023-05-01 18:48:39 +0530

    Db Setup with Sample API

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,11 +1,29 @@
+using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ProductsController: ControllerBase
+    public class ProductsController : ControllerBase
     {
-        
+        private StoreContext _Sdb { get; }
+        public ProductsController(StoreContext sdb)
+        {
+            _Sdb = sdb;
+        }
+        [HttpGet("GetProducts")]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await _Sdb.Products.ToListAsync();
+        }
+
+        [HttpGet("GetProduct/{Id}")]
+        public async Task<ActionResult<Product>> GetProduct(int Id)
+        {
+            return await _Sdb.Products.FindAsync(Id);
+        }
     }
 }
