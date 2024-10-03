ea24071f1b7ef7f8de39c8fa62f32e070a5dbeb6
shaharukhgit <shaharukhpathan786@gmail.com>
2024-10-03 22:10:13 +0530

    Angular Setup

--- API/obj/Debug/net6.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net6.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c34a3ce10f16ed624db61877b26fac33a40017ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5643f75c2c1246059cd058e2cd572d883ba1f6a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
